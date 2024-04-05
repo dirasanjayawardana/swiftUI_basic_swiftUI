9fb33d88f77eea69417d6d3ef25724652b72742a
Mona Lisa <andreno.juanda11@gmail.com>
2024-04-05 09:31:02 +0700

    Initial Commit

---
+++ BasicSwiftUi/BasicSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUiApp.swift
+//  BasicSwiftUi
+//
+//  Created by MACBOOK PRO on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
